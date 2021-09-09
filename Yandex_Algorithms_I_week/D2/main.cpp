d6c1a21b0e62ea969602527b18d8c7238b6783ab
CourteousSleet <me756478@gmail.com>
2021-09-09 11:00:12 +0300

    Added bin installation stage

--- Yandex_Algorithms_I_week/D2/main.cpp
+++ Yandex_Algorithms_I_week/D2/main.cpp
@@ -9,12 +9,12 @@ int main() {
   ofstream output_file("output.txt");
 
   if (input_file.is_open()) {
-    input_file >>
+    // input_file >>
     input_file.close();
   } else cerr << "Unable to open input file" << endl;
 
   if (output_file.is_open()) {
-    output_file << ;
+    // output_file << ;
     output_file.close();
   } else cerr << "Unable to open output file" << endl;
 
