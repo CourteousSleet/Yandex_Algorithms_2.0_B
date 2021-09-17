c8dc8dbdd8adbb3112acb3367ea6a5cf6118099d
CourteousSleet <me756478@gmail.com>
2021-09-17 16:24:15 +0300

    Added C4

--- Yandex_Algorithms_II_week/C4/main.cc
+++ Yandex_Algorithms_II_week/C4/main.cc
@@ -1,14 +1,40 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
+  map<string, int> words;
+  vector<pair<int, string>> sorted_words;
+
   ifstream input_file("input.txt");
   ofstream output_file("output.txt");
 
   if (input_file.is_open()) {
+    string s;
+    while (input_file >> s) {
+      words[s] += 1;
+    }
+    input_file.close();
+  } else cerr << "Unable to open input file" << endl;
+
+  for (auto &word: words) {
+    pair<int, string> kek = make_pair(word.second, word.first);
+    sorted_words.emplace_back(kek);
+  }
+
+  sort(sorted_words.begin(), sorted_words.end(), [](const pair<int, string> &x,
+                                                    const pair<int, string> &y) {
+    if (x.first == y.first) return x.second < y.second;
+    return x.first > y.first;
+  });
+
+  if (output_file.is_open()) {
+    for (auto &s_word: sorted_words) output_file << s_word.second << endl;
     output_file.close();
   } else cerr << "Unable to open output file" << endl;
 
--- Yandex_Algorithms_II_week/D4/main.cc
+++ Yandex_Algorithms_II_week/D4/main.cc
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
+#include <forward_list>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
+  map<string, int> words;
+  forward_list<map<string, int>> sorted_words;
+
   ifstream input_file("input.txt");
   ofstream output_file("output.txt");
 
   if (input_file.is_open()) {
+    input_file.close();
+  } else cerr << "Unable to open input file" << endl;
+
+  if (output_file.is_open()) {
     output_file.close();
   } else cerr << "Unable to open output file" << endl;
 
