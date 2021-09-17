841431425bf5b0176220f923034222f40730eda1
CourteousSleet <me756478@gmail.com>
2021-09-17 16:01:07 +0300

    Added B4

--- Yandex_Algorithms_II_week/B4/main.cc
+++ Yandex_Algorithms_II_week/B4/main.cc
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
 
 using namespace std;
 
 int main() {
+  map<string, long long int> elections;
   ifstream input_file("input.txt");
   ofstream output_file("output.txt");
 
   if (input_file.is_open()) {
+    string name;
+    long long int number_of_voters;
+    while (input_file >> name >> number_of_voters) {
+      elections[name] += number_of_voters;
+    }
+
+    for (auto &elected: elections) {
+      output_file << elected.first << ' ' << elected.second << endl;
+    }
+
     output_file.close();
   } else cerr << "Unable to open output file" << endl;
 
